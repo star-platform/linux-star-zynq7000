1b656cb03342725a5f9ea80503dcc56fcccd282f
alex <alex@alex-VirtualBox.(none)>
2015-01-03 11:52:41 +0800

    delete all file of linux_3.17

--- .missing-syscalls.d
+++
@@ -1,7 +0,0 @@
--:  /home/alex/share/star/linux-star-zynq7000/include/linux/kconfig.h \
- include/generated/autoconf.h \
- /home/alex/share/star/linux-star-zynq7000/arch/arm/include/asm/types.h \
- include/asm-generic/int-ll64.h \
- arch/arm/include/generated/asm/bitsperlong.h \
- include/asm-generic/bitsperlong.h \
- /home/alex/share/star/linux-star-zynq7000/arch/arm/include/asm/unistd.h
